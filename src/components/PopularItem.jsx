d3ddaa72f0fa80ee08cac4d9e7a5a2b6c50648e9
dpastoriza9 <dpastoriza@mobilecomputing.com.ar>
2023-03-31 11:44:49 -0300

    code update

--- src/components/PopularItem.jsx
+++ src/components/PopularItem.jsx
@@ -1,9 +1,8 @@
 const PopularItem = ({ img, top, title, text }) => {
-  const urlImgFolder = 'src/assets/images/';
 
   return (
     <div className="my-5 flex items-stretch border-b-[1px] border-white py-5 text-white last-of-type:border-y-0">
-      <img className="mr-5 aspect-[2/1] object-contain" src={urlImgFolder + img} />
+      <img className="mr-5 aspect-[2/1] object-contain" src={img} />
       <div className="flex flex-col">
         <span className="text-primary mb-3 text-5xl text-[#c9c8d0]">{top}</span>
         <h3 className="text-primary mb-3 text-2xl text-[#00001a]">{title}</h3>
